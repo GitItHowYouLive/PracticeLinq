9b2042879bb88eebd2aade80093000016f0c9f80
GitItHowYouLive <MicStandTerrence@gmail.com>
2022-05-04 02:08:54 -0500

    Initial Exercise Completion Commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var games = new List<string>() { "Beat Saber", "Superhot", "Pistol Whip", "Moss", "Walkabout Mini-Golf", "Thumper", "Tetris Effect", "I Expect You To Die", "Population One", "The Walking Dead: Saints and Sinners", "Gorn", "Robo Recall", "Resist", "Rec Room", "Keep Talking and Nobody Explodes", "The Climb", "Shooty Fruity"};
+            games = games.OrderBy(x => x.Length).ToList();
+            foreach(var game in games)
+            {
+                Console.WriteLine(game);
+            }
+        }
+    }
+}
